c6ecb66987d55fed0e68d822823f4c8c397fafe2
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-11-19 23:50:33 +0000

    making credentials changes

--- src/routes/local-credentials/+page.svelte
+++ src/routes/local-credentials/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { generateVideoToken } from "$dist/utils/util";
   import Zoom from "$dist/Zoom.svelte";
+  // make sure to create a file named credentials.ts with the following content:
+  // `export const sdk = { key: "...", secret: "..." }`
   import { sdk } from "./credentials";
   let name = $state(
     `User_${Math.random().toString(36).substring(2, 8)}_${navigator.userAgent}`
