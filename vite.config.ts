405c887a45d8c068959237df2f8b1d72bf03a981
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-11-25 21:18:03 +0000

    now that release name is in use, no longer need to override build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { server, plugins, build } from "./release/config/vite.js";
+import { server, plugins } from "./release/config/vite.js";
 
 export default defineConfig({
   plugins: [sveltekit(), ...plugins({ assetDir: "static" })],
   server,
-  build,
 });
