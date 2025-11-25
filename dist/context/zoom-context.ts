8d08a4a34eacb0445de53bfc9eb909c9c6803865
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-11-25 21:09:32 +0000

    deleting dist. instead using release

--- dist/context/zoom-context.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import type { ZoomClient } from '../index-types';
-export default React.createContext<ZoomClient>(null as any);
--- dist/feature/video/components/share-view/share-view-types.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ShareViewProps {
-  className?: string;
-  onRecieveSharingChange: (isSharing: boolean) => void;
-}
